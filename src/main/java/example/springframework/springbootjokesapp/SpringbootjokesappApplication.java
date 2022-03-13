df577ca61ede80d871b8ff8912ba536214c6e651
yerasoni20 <yerasoni20@gmail.com>
2022-03-13 07:48:16 -0400

    Created Spring Boot Jokes App

---
+++ src/main/java/example/springframework/springbootjokesapp/SpringbootjokesappApplication.java
@@ -0,0 +1,13 @@
+package example.springframework.springbootjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjokesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootjokesappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/springframework/springbootjokesapp/SpringbootjokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package example.springframework.springbootjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjokesappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
