229e4d36fe8906eff611f337586c4fa1af264246
yerasoni20 <yerasoni20@gmail.com>
2022-03-13 09:08:38 -0400

    Creating Controller

---
+++ src/main/java/example/springframework/springbootjokesapp/controller/JokeController.java
@@ -0,0 +1,25 @@
+package example.springframework.springbootjokesapp.controller;
+
+import example.springframework.springbootjokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model)
+    {
+        model.addAttribute("joke",jokeService.getJoke());
+        return "index";
+    }
+}
